26a688e07a31d793c5d9d94d8a8bb68b2359ca06
Your Name <akshita.srivastava.1505@gmail.com>
2025-11-15 21:25:45 +0530

    some more2

--- frontend/src/components/SplineScene.tsx
+++ frontend/src/components/SplineScene.tsx
@@ -6,7 +6,7 @@ import { Spotlight } from "./ui/spotlight";
  
 export function SplineSceneBasic() {
   return (
-    <Card className="w-full h-[500px] bg-black/[0.96] relative overflow-hidden">
+    <Card className="w-full h-[500px] bg-transparent relative overflow-hidden border-transparent shadow-none">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
