9838b55204ce96de9d1ce7825cb79c7d74769e4a
Your Name <akshita.srivastava.1505@gmail.com>
2025-11-15 21:23:48 +0530

    some more

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,29 +1,23 @@
 'use client';
 
 import Navigation from '@/components/Navigation';
-import { useEffect, useState } from 'react';
-
+import { SplineSceneBasic } from '@/components/SplineScene';
 
 export default function Home() {
- 
-
- 
-
-
   return (
     <div className="min-h-screen bg-gray-50">
       <Navigation />
       <div 
-  className="min-h-screen bg-repeat bg-center"
-  style={{ 
-    backgroundImage: "url('/images/Gemini_Generated_Image_71076g71076g7107.png')",
-    backgroundSize: "250px 250px"
-  }}
->
-  {/* Your content */}
-</div>
-
+        className="min-h-screen bg-repeat bg-center p-8"
+        style={{ 
+          backgroundImage: "url('/images/Gemini_Generated_Image_71076g71076g7107.png')",
+          backgroundSize: "250px 250px"
+        }}
+      >
+        <div className="container mx-auto pt-20">
+          <SplineSceneBasic />
         </div>
-      
+      </div>
+    </div>
   );
 }
